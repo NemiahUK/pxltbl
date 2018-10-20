4036870ecb95ef6416bff0c4edfa57fdd1e0641b
Steven Tomlinson <steven@nemiah.uk>
2018-10-20 23:54:37 +0100

    working on drop game

--- api/progs/drop.js
+++ api/progs/drop.js
@@ -16,14 +16,17 @@ var hasRun = false;
 var level = 1;
 var width = 9;
 
-var towerTop = 9;
+var towerTop = 0;
+var towerHeight = 0;
 var towerLeft = 7;
 
-var scroll = 0;
+var scroll = 11;
+
+var gravity = 0.5;
 
 
 var flyInX = 0.0-width;
-var flyInY = 2.0;
+var flyInY = 0.0;
 var flyInSpeed = 0.1;
 var flyInStatus = 1;       //0 - waiting   1 - flying in  2 - falling  3 - Landing
 var flyInHeight = 2;
@@ -40,9 +43,12 @@ var stack = [];
 stack.push({
     width: width,
     left: towerLeft,
-    height: flyInHeight,
-    color: flyInColor
+    height: 1,
+    color: { r:255, g:255, b:255}
 });
+
+
+var gibs = [];
 
 exports.loop = function(api) {
 
@@ -56,19 +62,42 @@ exports.loop = function(api) {
     //playing game
     api.blank(0, 0, 0);
 
+    //scroll tower/gibs if needed..
+    if(scroll - towerHeight < 5) scroll++;
+
+
 
     // Draw tower
+    towerHeight = 0;
     for (var i=0; i<stack.length; i++) {
+        towerHeight+= stack[i].height;
 
         api.setColor(stack[i].color.r, stack[i].color.g, stack[i].color.b);
-        api.fillBox(stack[i].left, (scroll+7)-2*i, stack[i].width, stack[i].height);
+        api.fillBox(stack[i].left, scroll-towerHeight, stack[i].width, stack[i].height);
     }
+
+    //draw gibs
+    for (var i=0; i<gibs.length; i++) {
+
+        api.setColor(Math.round(gibs[i].r),Math.round(gibs[i].g),Math.round(gibs[i].b),Math.round(gibs[i].a));
+        api.setPixel(Math.round(gibs[i].x), Math.round(gibs[i].y+scroll));
+
+        //movement
+        gibs[i].x+=gibs[i].vX;
+        gibs[i].y+=gibs[i].vY;
+        //gravity
+        gibs[i].vY+=gravity;
+        //fade
+        gibs[i].a-=16;
+
+    }
+
 
     switch (flyInStatus) {
         case 0:
 
             break;
-        case 1:
+        case 1: //flying in...
             //move flyin
             flyInX+=flyInSpeed;
 
@@ -79,18 +108,18 @@ exports.loop = function(api) {
             if(api.buttons.fire) flyInStatus++;
 
             break;
-        case 2:
+        case 2: //falling ...
             //move flyin
-            flyInY+=flyInSpeed;
+            flyInY+=gravity;
 
             //draw flyin
             api.setColor(flyInColor.r, flyInColor.g, flyInColor.b);
             api.fillBox(Math.round(flyInX),Math.round(flyInY),width,flyInHeight);
 
-            if(flyInY+flyInHeight >= scroll+towerTop) flyInStatus++;
+            if(flyInY+flyInHeight >= scroll-towerHeight) flyInStatus++;
 
             break;
-        case 3:
+        case 3:  //landed...
 
             flyInX = Math.round(flyInX);
             flyInY = Math.round(flyInY);
@@ -98,13 +127,33 @@ exports.loop = function(api) {
             //calculate new width
             if(flyInX < towerLeft) {
                 //too far left
+
+                //spawn gibs
+                for (var i=flyInX; i<towerLeft; i++) {
+                    for (var j=0; j<flyInHeight; j++) {
+                        spawnGib(i, flyInY+j - scroll, true, flyInColor.r, flyInColor.g, flyInColor.b);
+                    }
+                }
+
                 var oldWidth = width;
                 width = flyInX+width - towerLeft;
                 flyInX+=oldWidth-width;
 
+
+
             } else if(flyInX > towerLeft) {
                 //too far right
+
+                //spawn gibs
+                for (var i=towerLeft+width; i<flyInX+width; i++) {
+                    for (var j=0; j<flyInHeight; j++) {
+                        spawnGib(i, flyInY+j - scroll, false, flyInColor.r, flyInColor.g, flyInColor.b);
+                    }
+                }
+
                 width = towerLeft + width - flyInX;
+                towerLeft = flyInX;
+
 
             } else {
                 //bang on!
@@ -115,7 +164,7 @@ exports.loop = function(api) {
             }
 
             if(width < 1) {
-                api.playWav('looser');
+                //api.playWav('looser');
                 api.exit();
             }
 
@@ -136,20 +185,34 @@ exports.loop = function(api) {
                 width: width,
                 left: flyInX,
                 height: flyInHeight,
-                color: flyInColor
+                color: {
+                    r: flyInColor.r,
+                    g: flyInColor.g,
+                    b: flyInColor.b
+                }
             });
 
 
 
 
-
             flyInY=0;
-            flyInX=0-width;
+
             flyInStatus=1;
-            flyInSpeed+=0.02;
-            flyInColor.g+=50;
-            towerTop-=flyInHeight;
-            scroll+=2;
+            flyInSpeed*= -1;
+            if(flyInSpeed > 0) {
+                flyInSpeed+=0.01;
+                flyInX=0-width;
+            } else {
+                flyInSpeed-=0.01;
+                flyInX=api.pxlW;
+            }
+
+            flyInColor.g+=100;
+            flyInHeight=Math.ceil(Math.random() * Math.floor(3));
+
+
+
+
 
             break;
         case 5:
@@ -165,5 +228,26 @@ exports.loop = function(api) {
 
 
 
+};
+
+
+function spawnGib(x,y,left,r,g,b) {
+
+
+    var vX = -0.8 + (Math.random()*0.5);
+    var vY =  -2 + (Math.random()*1);
+
+    if(!left) vX*= -1;
+
+    gibs.push({
+        x: x,
+        y: y,
+        vX: vX,
+        vY: vY,
+        r: r,
+        g: g,
+        b: b,
+        a: 1
+    });
 };
 
