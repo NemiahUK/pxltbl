7156194853699449087028e6ac91ffc865f4264b
phyushin <phyushin@phyubox.com>
2018-10-31 12:20:19 +0000

    fixing conflict

--- api/progs/hackspace-logo.js
+++ api/progs/hackspace-logo.js
@@ -1,41 +1,9 @@
 // Hackspace logo Paul Williams https://github.com/phyushin.
-<<<<<<< HEAD
-<<<<<<< HEAD
-
 exports.setup = function(api) {
     api.blank(0, 0, 0);
 };
 
 exports.loop = function(api) {
-    api.blank(0,0,0);
-    api.setColor(255, 0, 0);
-=======
-exports.setup = function(api) {
-  api.blank(0, 0, 0);
-};
-
-exports.loop = function(api) {
-  api.blank(0,0,0);
-  api.setColor(255, 0, 0);
->>>>>>> 715a2976252b8c003c54910bed4c8441ad2195ea
-//draw a red box
-  api.fillBox(9,3,5,5);
-//draw a white H
-<<<<<<< HEAD
-    api.setColor(255,255,255);
-    api.fillBox(10,4,1,3);
-    api.fillBox(10,5,3,1);
-    api.fillBox(12,4,1,3);
-    // If fire button pressed then exit back to home screen.
-    if(api.buttons.any) api.exit();
-};
-
-=======
-exports.setup = function(api) {
-  api.blank(0, 0, 0);
-};
-
-exports.loop = function(api) {
   api.blank(0,0,0);
   api.setColor(255, 0, 0);
 //draw a red box
@@ -45,23 +13,9 @@ exports.loop = function(api) {
   api.fillBox(10,4,1,3);
   api.fillBox(10,5,3,1);
   api.fillBox(12,4,1,3);
-
-
-=======
-  api.setColor(255,255,255);
-  api.fillBox(10,4,1,3);
-  api.fillBox(10,5,3,1);
-  api.fillBox(12,4,1,3);
-
-
->>>>>>> 715a2976252b8c003c54910bed4c8441ad2195ea
+  // If fire button pressed then exit back to home screen.
   // If fire button pressed then exit back to home screen.
   if(api.buttons.any) {
     api.exit();
   }
-<<<<<<< HEAD
 };
->>>>>>> upstream/master
-=======
-};
->>>>>>> 715a2976252b8c003c54910bed4c8441ad2195ea
