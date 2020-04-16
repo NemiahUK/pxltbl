9cf8c88b3adc213ebb8aac94b004d2f5f538cb57
Steven Tomlinson <steven@nemiah.uk>
2020-04-16 19:30:56 +0100

    removed vagrant

--- vagrant-scripts/java.sh
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env bash
-
-# Needed for selenium and phantomjs testing
-apt-get install -y default-jre
--- vagrant-scripts/ubuntu.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-
-# Update apt-get
-apt-get update -y
-
-# Update Ubuntu
-apt-get -y upgrade
-apt-get -y dist-upgrade
