168312c8ab7f4ed61791bee90ddc79a9a0a0ef5e
Ricky Atkinson <ricky@nemiah.uk>
2020-09-03 14:26:30 +0100

    Updating web UI

---
+++ api/progs/portrait-hello.js
@@ -0,0 +1,28 @@
+// Hello world example prog.
+
+// Put variables here that you need to persist between loops.
+var message = 'Hi =]';
+
+exports.setup = function(api) {
+    // Code here get's executed once at program startup.
+
+    // Rotate to portrait
+    api.setRotation(90);
+
+    // Blank the screen
+    api.blank(0, 0, 0);
+};
+
+exports.loop = function(api) {
+
+    // Set the drawing colour.
+    api.setColor(0, 255, 0);
+
+    // Print some text.
+    api.text(message, 1,2);
+
+    // If fire button pressed then exit back to home screen.
+    if(api.buttons.any) api.exit();
+
+};
+
