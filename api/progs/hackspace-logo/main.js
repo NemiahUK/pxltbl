de594b4f80b7fad598cacbedca41593d6047687d
Ricky Atkinson <ricky@nemiah.uk>
2020-09-15 15:49:51 +0100

    :construction: Working on hackspace logo demo

--- api/progs/hackspace-logo/main.js
+++ api/progs/hackspace-logo/main.js
@@ -5,11 +5,11 @@ exports.setup = function(api) {
 
 exports.loop = function(api) {
   api.blank(0,0,0);
-  api.setColor(255, 0, 0);
+  api.setDrawColor(255, 0, 0);
 //draw a red box
   api.fillBox(9,3,5,5);
 //draw a white H
-  api.setColor(255,255,255);
+  api.setDrawColor(255,255,255);
   api.fillBox(10,4,1,3);
   api.fillBox(10,5,3,1);
   api.fillBox(12,4,1,3);
@@ -22,9 +22,8 @@ exports.loop = function(api) {
     }
   }
 
-
   // If fire button pressed then exit back to home screen.
-  if(api.buttons.any) {
+  if(api.getButtons().any) {
     api.exit();
   }
 };
