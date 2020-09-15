d2247e3b726564cb6b490ff885a5b46c35e90450
Ricky Atkinson <ricky@nemiah.uk>
2020-09-15 09:59:15 +0100

    :construction: Progs.

--- api/api.js
+++ api/api.js
@@ -1535,7 +1535,7 @@ const pxlTbl = ( function() {
                 + Math.round(b / 255 * 5);
         }
 
-        hslToRgb = (h, s, l) => {
+        hslToRgb = (h, s, l, a = 1) => {
             let r = 0;
             let g = 0;
             let b = 0;
@@ -1559,9 +1559,9 @@ const pxlTbl = ( function() {
                 const q = l < 0.5 ? l * (1 + s) : l + s - l * s;
                 const p = 2 * l - q;
 
-                r = hue2rgb(p, q, h + 1/3);
+                r = hue2rgb(p, q, h + 1 / 3);
                 g = hue2rgb(p, q, h);
-                b = hue2rgb(p, q, h - 1/3);
+                b = hue2rgb(p, q, h - 1 / 3);
             }
 
             return [ Math.round(r * 255), Math.round(g * 255), Math.round(b * 255) ];
--- api/progs/nemiah/main.js
+++ api/progs/nemiah/main.js
@@ -1,15 +1,15 @@
-var rainbow = [0,45,65,135,210,260,270];
+const rainbow = [0,45,65,135,210,260,270];
 
-var xPos = 0;
-var yPos;
-var xScroll = 5;
+let xPos = 0;
+let yPos = 0;
+let xScroll = 5;
 
-var animState = 0;
+let animState = 0;
 
 exports.setup = function(api) {
     api.fpsLimit = 60;
-    api.setRotation(0);
-    yPos = Math.floor((api.pxlH - 8)/2);
+    api.setOrientation(0);
+    yPos = Math.floor((api.pxlH - 8) / 2);
 };
 
 
@@ -17,34 +17,40 @@ exports.setup = function(api) {
 exports.loop = function(api) {
     api.blank(0, 0, 0);
 
-    var y = yPos;
-    if(xPos%6 < 3) y=yPos-1;
+    let y = yPos;
+    if(xPos % 6 < 3) y = yPos - 1;
 
-    api.setColor(255,255,255);
-    api.fillBox(xPos-xScroll,1+y,7,7);
-    api.setColor(0, 0, 0);
-    api.fillBox(xPos+3-xScroll,4+y,1,4);
-    api.text('nemiah', xPos+1-xScroll,y);
+    api.setDrawColor(255, 255, 255);
+    api.fillBox(xPos - xScroll, 1 + y, 7, 7);
+    api.setDrawColor(0, 0, 0);
+    api.fillBox(xPos + 3 - xScroll, 4 + y, 1, 4);
+    api.text('nemiah', xPos + 1 - xScroll, y);
 
 
-    for (var j=0; j < 8; j++) {
-        for (var i = 0; i < 7; i++) {
-            var y = yPos;
-            if((xPos-j)%6 < 3) y--;
-            api.setColorHsl(rainbow[i], 255, 128, 1-j/9);
-            api.setPixel(xPos - j - 1-xScroll, 1 + i + y);
+    for (let j = 0; j < 8; j++) {
+        for (let i = 0; i < 7; i++) {
+            let y = yPos;
+            if((xPos - j) % 6 < 3) y--;
+
+            let colorRgb = api.hslToRgb(rainbow[i], 255, 128);
+
+            //api.setDrawColor(colorRgb.r, colorRgb.g, colorRgb.b);
+            api.setDrawColorHsl({ h: rainbow[i], s: 255, l: 128, a: 1-j/9 });
+            //api.setDrawColor(255, 255, 255);
+            api.setPixel(2, 2);
+            //api.setPixel(xPos - j - (1 - xScroll), 1 + i + y);
         }
     }
 
 
     if(animState === 0) {
-        xPos+=0.2;
+        xPos += 0.2;
     } else {
-        xPos+=1;
+        xPos += 1;
     }
     if(xPos >= (api.pxlW+10)/2 && animState === 0) xScroll+=0.2;
 
-    if(api.buttons.any || api.getTouch().length) {
+    if(api.getButtons().any || api.getTouch().length) {
         animState=1;
     }
 
